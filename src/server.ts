7d66deb9c845008af65796fbe6b89d5b265f6e5d
Leonel Gauna <leonel.mariano.g@gmail.com>
2021-03-02 17:40:53 -0300

    Create POST route handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
 
 const app = express();
 const PORT: number = 3000;
 
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   res.send('hello world!');
+});
+
+app.post('/', (req, res) => {
+  res.send(req.body);
 });
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}...`));
